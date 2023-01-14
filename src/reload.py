ac6ceb796659b82bd705c475c422dd0a3675dab0
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2023-01-14 23:30:50 +0300

    v2 Pass input
    Password input added, Exception handler added

--- src/reload.py
+++ src/reload.py
@@ -1,4 +1,11 @@
-from requests import post
+import requests
+import base64
 
-x = post('http://192.168.0.1/login/Auth', data={"password":"MjIwNjIwMDU="}, allow_redirects=False)
-post("http://192.168.0.1/goform/sysReboot", data={"module1":"sysOperate","action":"reboot"}, headers={"cookie":x.headers["Set-Cookie"].split(";")[0]})
+try:
+	password = base64.b64encode(bytes(input("For restart input password: "), "utf-8")).decode("utf-8")
+	x = requests.post('http://192.168.0.1/login/Auth', data={"password":password}, allow_redirects=False)
+	requests.post("http://192.168.0.1/goform/sysReboot", data={"module1":"sysOperate","action":"reboot"}, headers={"cookie":x.headers["Set-Cookie"].split(";")[0]})
+except:
+	print("Произошла ошибка. Проверьте правильность ввода пароля и / или повторите попытку позже.")
+	quit()
+print("Роутер перезагружется. Пожалуйста ожидайте включения.")
